24ed112058d12a26b4c424984e9dab86393b4de7
firzen92 <vinay.potaraju@gmail.com>
2021-06-15 19:40:12 +0530

    change 1
    auth component changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+
+  isLogin = true;
+
+  constructor() {}
+
+  switchToggle() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+  }
+}
