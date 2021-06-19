4781ed25c525457091fce0b2bd83122714f1b9ce
firzen92 <vinay.potaraju@gmail.com>
2021-06-19 12:31:57 +0530

    signup and signin
    signup and signin API

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,45 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
 })
 export class AuthComponent {
-
   isLogin = true;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   switchToggle() {
     this.isLogin = !this.isLogin;
   }
 
   onSubmit(f: NgForm) {
-    console.log(f);
+    let payload = {...f.value, returnSecureToken: true};
+    if(!this.isLogin) {
+      this.http
+        .post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDLU9Z0QR9hwCW87aDW7aG_yHibhikKuaM",
+          payload
+        )
+        .subscribe((res) => {
+          console.log(res);
+        }, err => {
+          console.log(err);
+        });
+    } else {
+      this.http
+        .post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDLU9Z0QR9hwCW87aDW7aG_yHibhikKuaM",
+          payload
+        )
+        .subscribe((res) => {
+          console.log(res);
+        }, err => {
+          console.log(err);
+        });
+    }
+    f.reset();
   }
 }
